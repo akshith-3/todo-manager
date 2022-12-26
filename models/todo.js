f18dce9ebb17ffcac8059e8da3c95e3a59cc0b81
akshith <gakshithrao1663@gmail.com>
2022-12-26 17:21:13 +0530

    Level-7

--- models/todo.js
+++ models/todo.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
@@ -11,12 +13,64 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
 
+    static getTodos(){
+      return this.findAll();
+    }
+
+    static async showList(){
+      console.log("My Todo list \n");
+
+      console.log("Overdue");
+      console.log((await Todo.overdue()).map((x) => { x.displayableString();}).join("\n"));
+
+      console.log("\n");
+      
+      console.log("Due Today");
+      console.log((await Todo.overdue()).map((x) => { x.displayableString();}).join("\n"));
+
+      console.log("\n");
+
+      console.log("Due Later");
+      console.log((await Todo.overdue()).map((x) => { x.displayableString();}).join("\n"));
+      
+    }
+
+    static async overdue(){
+      return await Todo.findAll({
+        where:{
+          dueDate: { [Op.lt]: new Date().toLocaleDateString("en-CA")},
+        },
+      });
+    }
+
+    static async dueToday(){
+      return await Todo.findAll({
+        where:{
+          dueDate: { [Op.eq]: new Date().toLocaleDateString("en-CA")},
+        },
+      });
+    }
+
+    static async dueLater(){
+      return await Todo.findAll({
+        where:{
+          dueDate: { [Op.gt]: new Date().toLocaleDateString("en-CA")},
+        },
+      });
+    }
+   
+
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
-
+    
     markAsCompleted() {
       return this.update({ completed: true });
+    }
+
+    displayableString(){
+      let checkbox=this.completed ? "[x]" : "[ ]";
+      return `${this.id}.${checkbox} ${this.title} ${this.dueDate ==new Date().toLocaleDateString("en-CA") ? "" : this.dueDate}`.trim();
     }
   }
   Todo.init(
